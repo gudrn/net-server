c210d53009f2cd2021128efbedc8398eebec30ca
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-10-25 21:03:23 +0900

    멀티플레이어 가능하게 수정

--- src/handlers/user/locationUpdate.handler.js
+++ src/handlers/user/locationUpdate.handler.js
@@ -1,11 +1,43 @@
 import { getGameSession } from '../../session/game.session.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+import { HANDLER_ID, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
 
 const locationUpdateHandler = async ({ socket, userId, payload }) => {
-  const { x, y } = payload;
+  const { x, y, deviceId } = payload;
   const gameSession = getGameSession();
   gameSession.updateUserPosition(socket, x, y);
-  let user = gameSession.getUser(socket);
-  console.log(user.x, user.y);
+
+  const protoMessages = getProtoMessages();
+  const allUsers = gameSession.getUsers();
+  const LocationUpdate = protoMessages.gameNotification.LocationUpdate;
+  const userLocation = LocationUpdate.create({
+    users: allUsers
+      .filter((user) => user.socket !== socket)
+      .map((user) => ({
+        id: user.id,
+        playId: user.playerId,
+        x: user.x,
+        y: user.y,
+      })),
+  });
+
+  const buffer = LocationUpdate.encode(userLocation).finish();
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    buffer.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(PACKET_TYPE.LOCATION, 0);
+
+  const response = Buffer.concat([packetLength, packetType, buffer]);
+
+  // 모든 유저에게 위치 업데이트 전송
+  socket.write(response);
 };
 
 export default locationUpdateHandler;
